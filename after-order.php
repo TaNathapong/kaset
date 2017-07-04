eb889203fe3ba98fb48d57a75d189537de38e4ea
mario <panuwat_koktomchai@hotmail.com>
2017-07-04 17:04:34 +0700

    update

---
+++ after-order.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>eElectronics - HTML eCommerce Template</title>
+    
+    <!-- Google Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
+    
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<style>
+		.mySlides {display:none}
+		.demo {cursor:pointer}
+	</style>
+		
+
+  </head>
+  <body>
+<?php include'header.php' ?>
+
+<div class="product-big-title-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="product-bit-title text-center">
+                        <h2>รายละเอียดการส</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- start images slider detail -->
+<div style="margin: 20px 20px 20px 20px;">
+	<div class="row">
+			<div class="col-md-3">
+				<div style="width: 100%;border:solid 2px; border-color: #d9d9d9;">
+					<div class="w3-content" style="max-width:1200px">
+					  <img class="mySlides" src="img/product-1.jpg" style="width:100%">
+					  <img class="mySlides" src="img/product-2.jpg" style="width:100%">
+					  <img class="mySlides" src="img/product-3.jpg" style="width:100%">
+					  <img class="mySlides" src="img/product-4.jpg" style="width:100%">
+
+					  <div class="w3-row-padding w3-section">
+					    <div class="w3-col s3">
+					      <img class="demo w3-opacity w3-hover-opacity-off" src="img/product-1.jpg" style="width:100%" onclick="currentDiv(1)">
+					    </div>
+					    <div class="w3-col s3">
+					      <img class="demo w3-opacity w3-hover-opacity-off" src="img/product-2.jpg" style="width:100%" onclick="currentDiv(2)">
+					    </div>
+					    <div class="w3-col s3">
+					      <img class="demo w3-opacity w3-hover-opacity-off" src="img/product-3.jpg" style="width:100%" onclick="currentDiv(3)">
+					    </div>
+					    <div class="w3-col s3">
+					      <img class="demo w3-opacity w3-hover-opacity-off" src="img/product-4.jpg" style="width:100%" onclick="currentDiv(4)">
+					    </div>
+					  </div>
+					</div>
+				</div>
+					
+		</div>
+		<div class="col-md-6" style="border: solid 2px; border-color: #d9d9d9; ">
+			<h3>แจ้งรายการสั่งสื้อสินค้าที่สั่งซื้อ</h3>
+			<p>
+				สีเขียวในผักและผลไม้มาจากเม็ดสีของสารที่มีชื่อว่า คลอโรฟิลด์ (Chlorophyll) โดยจะมีตั้งแต่เขียวเข้มจัด ได้แก่ คะน้า สาหร่ายบางชนิด ตำลึง ผักใบเขียวต่างๆ และสีเขียวแบบทั่วไป เช่น แอปเปิ้ลเขียว ฝรั่ง ผักกาด</p>
+
+				<p>	ซึ่งในผักสีเขียวที่มี คลอโรฟิลด์ (Chlorophyll) นี้ เต็มไปด้วยสารต้านอนุมูลอิสระ ช่วยในการต่อต้านโรคมะเร็ง ทำให้ผิวพรรณเปล่งปลั่งสดใส ช่วยยับยั้งการเกิดริ้วรอย นอกจากนี้การทานผักใบเขียวเป็นประจำจะช่วยให้การขับถ่ายดี ลดอากาท้องผูก เนื่องจากผักเหล่านี้มีกากใยสูงมีส่วนช่วยในการลดน้ำหนัก เนื่องจากให้พลังงานต่ำ
+			</p>
+			<div class="product-option-shop" style="margin-bottom: 10px;">
+                <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id="70" rel="nofollow" href="/canvas/shop/?add-to-cart=70">พิมพ์ใบเสร็จ</a>
+                <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id="70" rel="nofollow" href="/canvas/shop/?add-to-cart=70">แจ้งโอน</a>
+			</div>
+		</div>
+		<div class="col-md-3">
+			
+		</div>
+	</div>	
+	
+</div>
+    <!-- end images slider detail -->
+
+
+<!-- Latest jQuery form server -->
+    <script src="https://code.jquery.com/jquery.min.js"></script>
+    
+    <!-- Bootstrap JS form CDN -->
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    
+    <!-- jQuery sticky menu -->
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    
+    <!-- jQuery easing -->
+    <script src="js/jquery.easing.1.3.min.js"></script>
+    
+    <!-- Main Script -->
+    <script src="js/main.js"></script>
+  </body>
+</html>
+
+
+<script>
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " w3-opacity-off";
+}
+</script>
