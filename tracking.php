520b82119d7544ae3ed5093decf0f53d568e7df3
mario <panuwat_koktomchai@hotmail.com>
2017-07-06 11:12:34 +0700

    แก้ใขไฟล์ header มีโค๊ดบางส่วนซ้ำกัน

--- tracking.php
+++ tracking.php
@@ -28,30 +28,31 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+      <style type="text/css">
+            .boxFollow{
+                border-left: solid 5px;
+                border-top: solid 3px;
+                border-color:  #1abc9c;
+                border-radius: 7px;
+                margin-top: 20px;
+                margin-left: 20px;
+                padding-left: 20px;
+            }
+            .textFollow{
+                font-size: 18px;
+
+            }
+            .textlike:hover{
+                border-bottom: solid 5px;
+
+            }
+        </style>
   </head>
   <body>
    
     
     <?php include'header.php' ?>
-    <style type="text/css">
-        .boxFollow{
-            border-left: solid 5px;
-            border-top: solid 3px;
-            border-color:  #1abc9c;
-            border-radius: 7px;
-            margin-top: 20px;
-            margin-left: 20px;
-            padding-left: 20px;
-        }
-        .textFollow{
-            font-size: 18px;
-
-        }
-        .textlike:hover{
-            border-bottom: solid 5px;
-
-        }
-    </style>
+    
     <div class="product-big-title-area">
         <div class="container">
             <div class="row">
