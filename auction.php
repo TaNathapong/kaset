8a397042290157a10cf107daac4ea1e2f435fad1
Wipada <vipada.se@gmail.com>
2017-07-10 21:51:40 +0700

    add auction-announ..,auction page

---
+++ auction.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ตลาด - GreenHouse</title>
+
+    <!-- Google Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Kanit' rel="stylesheet" type='text/css'>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="css/flipclock.css">
+    <link rel="stylesheet" href="css/flipclock.scss">
+
+    <!-- timer -->
+    <link rel="stylesheet" href="../compiled/flipclock.css">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="../compiled/flipclock.js"></script>
+</head>
+
+<body>
+
+    <div class="header-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4"></div>
+                <div class="col-md-8">
+                    <div class="user-menu text-right">
+                        <ul>
+                            <li><a href="#"><i class="fa fa-user"></i> บัญชีของฉัน</a></li>
+                            <li><a href="#"><i class="fa fa-user"></i> เข้าสู่ระบบ</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- End header area -->
+
+    <div class="mainmenu-area">
+        <div class="container">
+            <div class="row">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="index.php">หน้าแรก</a></li>
+                        <li><a href="shop.php">ตลาด</a></li>
+                        <li><a href="travel.html">ท่องเที่ยว</a></li>
+                        <li><a href="contract.html">ติดต่อเรา</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div> <!-- End mainmenu area -->
+
+    <div class="product-big-title-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="logo">
+                        <h1><a href="index.php">Green<span>House</span></a></h1>
+                    </div>
+                </div>
+
+                <div class="col-sm-6">
+                    <div class="shopping-item">
+                        <a href="auction-detail.html">Cart - <span class="cart-amunt">$800</span> <i class="fa fa-shopping-cart"></i> <span class="product-count">5</span></a>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12" >
+                    <div class="product-bit-title text-center">
+                        <h2>สินค้าประมูล</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="shop-area">
+        <div class="container">
+            <center><h2>สินค้า</h2></center>
+            <div class="col-md-3">
+                <div class="category-list">
+                    <div class="col-md-12 col-xs-6">
+                        <center><h3>ประเภทสินค้า</h3></center>
+                    </div>
+                    <ul class="w3-ul w3-border col-md-12 col-xs-6">
+                        <li><a href="">ทั้งหมด</a></li>
+                        <li><a href="">ผัก</a></li>
+                        <li><a href="">ผลไม้</a></li>
+                        <li><a href="">OTOP</a></li>
+                        <li><a href="">Premium</a></li>
+                        <li><a href="">อุปกรณ์ทางการเกษตร</a></li>
+                        <li><a href="">สินค้าประมูล</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-md-9">
+                <!-- gallery -->
+                <div class="row" style="padding:10px;">
+                    <div class="latest-product">
+                        <div class="product-carousel">
+                            <div class="single-product">
+                                <div class="product-f-image">
+                                    <img src="img/coffee4.png" style="max-width:1200px">
+                                    <div class="product-hover">
+                                        <a href="auction-detail.html" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> ประมูลเลย</a>
+                                    </div>
+                                </div>
+
+                                <h2><a href="show-products.html">เมล็ดกาแฟขี้ชะมด</a></h2>
+
+                                <div class="promo-price">
+                                    <ins>ราคาประมูล ณ ปัจจุบัน :</ins></br>
+                                    <ins>฿1000.00</ins>
+                                </div>
+                            </div>
+                            <div class="single-product">
+                                <div class="product-f-image">
+                                    <img src="img/green-tea.jpg" alt="">
+                                    <div class="product-hover">
+                                        <a href="auction-detail.html" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> ประมูลเลย</a>
+                                    </div>
+                                </div>
+
+                                <h2><a href="show-products.html">ชาเขียว</a></h2>
+                                <div class="promo-price">
+                                    <ins>ราคาประมูล ณ ปัจจุบัน :</ins></br>
+                                    <ins>฿2500.00</ins>
+                                </div>
+                            </div>
+                            <div class="single-product">
+                                <div class="product-f-image">
+                                    <img src="img/durian-300.jpg" alt="">
+                                    <div class="product-hover">
+                                        <a href="auction-detail.html" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> ประมูลเลย</a>
+                                    </div>
+                                </div>
+
+                                <h2><a href="show-products.html">ทุเรียนหมอนทอง</a></h2>
+
+                                <div class="promo-price">
+                                    <ins>ราคาประมูล ณ ปัจจุบัน :</ins></br>
+                                    <ins>฿996.00</ins>
+                                </div>
+                            </div>
+                            <div class="single-product">
+                                <div class="product-f-image">
+                                    <img src="img/botun.jpg" alt="">
+                                    <div class="product-hover">
+                                        <a href="auction-detail.html" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> ประมูลเลย</a>
+                                    </div>
+                                </div>
+
+                                <h2><a href="show-products.html"ดอกโบตั่น</a></h2>
+
+                                <div class="promo-price">
+                                    <ins>ราคาประมูล ณ ปัจจุบัน :</ins></br>
+                                    <ins>฿3700.00</ins>
+                                </div>
+                            </div>
+                            <div class="single-product">
+                                <div class="product-f-image">
+                                    <img src="img/rose154.jpg" alt="">
+                                    <div class="product-hover">
+                                        <a href="auction-detail.html" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> ประมูลเลย</a>
+                                    </div>
+                                </div>
+
+                                <h2><a href="show-products.html">ดอกกุหลาบคุนหมิง สีเหลือง</a></h2>
+
+                                <div class="promo-price">
+                                    <ins>ราคาประมูล ณ ปัจจุบัน :</ins></br>
+                                    <ins>฿1200.00</ins>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End gallery -->
+
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-top-area">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-6">
+                    <div class="footer-about-us">
+                        <h2>e<span>Electronics</span></h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis sunt id doloribus vero quam laborum quas alias dolores blanditiis iusto consequatur, modi aliquid eveniet eligendi iure eaque ipsam iste, pariatur omnis sint! Suscipit, debitis, quisquam. Laborum commodi veritatis magni at?</p>
+                        <div class="footer-social">
+                            <a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
+                            <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
+                            <a href="#" target="_blank"><i class="fa fa-youtube"></i></a>
+                            <a href="#" target="_blank"><i class="fa fa-linkedin"></i></a>
+                            <a href="#" target="_blank"><i class="fa fa-pinterest"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6">
+                    <div class="footer-menu">
+                        <h2 class="footer-wid-title">User Navigation </h2>
+                        <ul>
+                            <li><a href="">My account</a></li>
+                            <li><a href="">Order history</a></li>
+                            <li><a href="">Wishlist</a></li>
+                            <li><a href="">Vendor contact</a></li>
+                            <li><a href="">Front page</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6">
+                    <div class="footer-menu">
+                        <h2 class="footer-wid-title">Categories</h2>
+                        <ul>
+                            <li><a href="">Mobile Phone</a></li>
+                            <li><a href="">Home accesseries</a></li>
+                            <li><a href="">LED TV</a></li>
+                            <li><a href="">Computer</a></li>
+                            <li><a href="">Gadets</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6">
+                    <div class="footer-newsletter">
+                        <h2 class="footer-wid-title">Newsletter</h2>
+                        <p>Sign up to our newsletter and get exclusive deals you wont find anywhere else straight to your inbox!</p>
+                        <div class="newsletter-form">
+                            <input type="email" placeholder="Type your email">
+                            <input type="submit" value="Subscribe">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="copyright">
+                        <p>&copy; 2015 eElectronics. All Rights Reserved. Coded with <i class="fa fa-heart"></i> by <a href="http://wpexpand.com" target="_blank">WP Expand</a></p>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="footer-card-icon">
+                        <i class="fa fa-cc-discover"></i>
+                        <i class="fa fa-cc-mastercard"></i>
+                        <i class="fa fa-cc-paypal"></i>
+                        <i class="fa fa-cc-visa"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Latest jQuery form server -->
+    <script src="https://code.jquery.com/jquery.min.js"></script>
+
+    <!-- Bootstrap JS form CDN -->
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+    <!-- jQuery sticky menu -->
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+
+    <!-- jQuery easing -->
+    <script src="js/jquery.easing.1.3.min.js"></script>
+
+    <!-- Main Script -->
+    <script src="js/main.js"></script>
+
+    <!-- coundown timer-->
+    <script src="js/flipclock.js"></script>
+    <script src="js/flipclock.min.js"></script>
+
+    <!-- clock Script -->
+    <script type="text/javascript">
+    var clock;
+
+    $(document).ready(function() {
+
+        clock = $('.clock').FlipClock(1200, {
+            countdown: true
+        });
+    });
+    </script>
+
+</body>
+</html>
